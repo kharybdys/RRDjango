e64522cedec6ef01ea0488d2859ab90a41738e7b
Kharybdys <kharybdys@gmail.com>
2022-12-30 22:33:16 +0100

    Add tests based on movement cards

--- tests/roborally_tests/game/data.py
+++ tests/roborally_tests/game/data.py
@@ -19,10 +19,15 @@ def to_movable_movement_and_expectation(movable_element_dict: dict) -> (Movable,
     del movable_element_dict["type"]
     expectation_dict = movable_element_dict.pop("expectation", None)
     movement_dict = movable_element_dict.pop("movement", None)
+    movement_cards = movable_element_dict.pop("movement_cards", None)
     # Create the movable
     facing_direction = to_optional_direction(movable_element_dict.get("facing_direction", None))
     movable_element_model = MovableModelMock(**dict(movable_element_dict, facing_direction=facing_direction))
     movable = cls(movable_element_model)
+    # Handle cards if applicable
+    if movement_cards:
+        for movement_card_dict in movement_cards:
+            movable_element_model.add_card(movement_card_dict["round"], movement_card_dict["phase"], movement_card_dict["status"], movement_card_dict["card_definition"])
     # Handle movement if applicable
     if movement_dict:
         direction = to_optional_direction(movement_dict.get("direction", None))
--- tests/roborally_tests/mocks.py
+++ tests/roborally_tests/mocks.py
@@ -1,8 +1,17 @@
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 
 from roborally.game.card import CardDefinition
 from roborally.game.movable import Movable
 from roborally.game.direction import Direction
+from roborally.game.movement import Movement
+
+
+@dataclass
+class MovementCardMock:
+    round: int
+    phase: int
+    status: str
+    card_definition: str
 
 
 @dataclass
@@ -17,12 +26,22 @@ class CoordinatesWithDirection(Coordinates):
 
 
 @dataclass
-@dataclass
 class MovableModelMock(CoordinatesWithDirection):
     order_number: int = 0
+    movement_cards: list[MovementCardMock] = field(default_factory=list)
 
     def save(self):
         pass
+
+    def add_card(self, round, phase, status, card_definition):
+        self.movement_cards.append(MovementCardMock(round, phase, status, card_definition))
+
+    def get_movements_for(self, round, phase, movable) -> list[Movement]:
+        return [movement
+                for card
+                in filter(lambda c: c.round == round and c.phase == phase and c.status in ['FINAL', 'LOCKED'], self.movement_cards)
+                for movement
+                in Movement.from_card_definition(movable, CardDefinition(card.card_definition))]
 
 
 @dataclass
